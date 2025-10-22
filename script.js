fa67ff3ead5c8179ba7880d0efe92fd7a288f705
ruiz-777 <216435410+ruiz-777@users.noreply.github.com>
2025-10-22 10:45:03 -0300

    Create a prompt to user choose an option
    Create a prompt to user choose an option
    Log the option in console
    Add comments to organize the code

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let x = getComputerNumber ();
 
+// Computer choosing an option
+
+// 1. Computer choosing a random number
+
 function getComputerNumber(){
     return Math.random()*100
 }
 
+// 2. Computer transforming this random number in game option
+
 function getComputerChoice(number){
     if (number < 33.33) {
-       return "Rock";
+       return "Computer chooses Rock";
     } else if (number > 33.33 && number < 66.66) {
-        return "Paper";
+        return "Computer chooses Paper";
     } else {
-        return "Scissor";
+        return "Computer chooses Scissor";
     }
 
 }
 
 console.log (getComputerChoice(x));
+
+// User choosing an option
+
+// 1. Browser asking the option for the user
+
+let userOption = prompt("Choose an option between Rock, Paper or Scissor.","Choose an option");
+
+console.log ("User chooses " + userOption);
+
