b1d9ad6a6b581751f6bfa2bc3cf99a365f3efd33
ruiz-777 <216435410+ruiz-777@users.noreply.github.com>
2025-10-22 13:20:52 -0300

    Create the logic to play one round
    Create the basic logic to play one round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let x = getComputerNumber ();
 
-// Computer choosing an option
+// COMPUTER CHOOSING AN OPTION
 
 // 1. Computer choosing a random number
 
@@ -12,18 +12,18 @@ function getComputerNumber(){
 
 function getComputerChoice(number){
     if (number < 33.33) {
-       return "Computer chooses Rock";
+       return "Rock";
     } else if (number > 33.33 && number < 66.66) {
-        return "Computer chooses Paper";
+        return "Paper";
     } else {
-        return "Computer chooses Scissor";
+        return "Scissor";
     }
 
 }
 
-console.log (getComputerChoice(x));
+console.log ("Computer chooses " + getComputerChoice(x));
 
-// User choosing an option
+// USER CHOOSING AN OPTION
 
 // 1. Browser asking the option for the user
 
@@ -31,3 +31,33 @@ let userOption = prompt("Choose an option between Rock, Paper or Scissor.","Choo
 
 console.log ("User chooses " + userOption);
 
+// SCORE
+
+// 1. Score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+// 2. Logic to play a single round
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "What a boring game, you guys tied!";
+    } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        return "Human Wins. Rock beats Scissor";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        return "Human Wins. Paper beats Rock";
+    } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
+        return "Human Wins. Scissor beats Paper";
+    }  else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        return "Computer Wins. Paper beats Rock";
+    } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        return "Computer Wins. Scissor beats Paper";
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        return "Computer Wins. Rock beats Scissor";
+    } else {
+        return "Invalid";
+    }
+}
+
+console.log (playRound (userOption, (getComputerChoice(x))));
