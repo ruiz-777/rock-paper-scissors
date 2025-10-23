bff402f59356568d2b75510e76047111ad4f57ad
ruiz-777 <216435410+ruiz-777@users.noreply.github.com>
2025-10-23 09:47:55 -0300

    Make the loop to play five times
    Make the loop to play five times
    Create the overall winner function
    Refactor some outputs to be more user friendly
    Refactor some lines to be easier to code

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
+for (i=1; i<=5; i++){
+
 let x = getComputerNumber ();
 
 // COMPUTER CHOOSING AN OPTION
@@ -21,8 +26,6 @@ function getComputerChoice(number){
 
 }
 
-console.log ("Computer chooses " + getComputerChoice(x));
-
 // USER CHOOSING AN OPTION
 
 // 1. Browser asking the option for the user
@@ -35,14 +38,14 @@ function capitalize (userOption) {
     return userOption.charAt(0).toUpperCase() + userOption.slice(1).toLowerCase();
 }
 
-console.log ("User chooses " + capitalize (userOption));
+console.log (i + "." + " You: " + capitalize (userOption) + " | Computer: " + getComputerChoice(x));
 
 // SCORE
 
 // 1. Score variables
 
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
 
 // 2. Logic to play a single round
 
@@ -50,19 +53,19 @@ function playRound (humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return "What a boring game, you guys tied!";
     } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
-        return "Human Wins. Rock beats Scissor";
+        return "You win. Rock beats Scissor";
     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        return "Human Wins. Paper beats Rock";
+        return "You win. Paper beats Rock";
     } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
-        return "Human Wins. Scissor beats Paper";
+        return "You win. Scissor beats Paper";
     }  else if (humanChoice === "Rock" && computerChoice === "Paper") {
-        return "Computer Wins. Paper beats Rock";
+        return "You lose. Paper beats Rock";
     } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
-        return "Computer Wins. Scissor beats Paper";
+        return "You lose. Scissor beats Paper";
     } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
-        return "Computer Wins. Rock beats Scissor";
+        return "You lose. Rock beats Scissor";
     } else {
-        return "Invalid";
+        return "You typed anything wrong!";
     }
 }
 
@@ -71,14 +74,14 @@ console.log (playRound (capitalize (userOption), (getComputerChoice(x))));
 // 3. Scoring a single round
 
 function score (winner) {
-    if (winner === "Human Wins. Rock beats Scissor" || 
-        winner === "Human Wins. Paper beats Rock" ||
-        winner === "Human Wins. Scissor beats Paper") {
+    if (winner === "You win. Rock beats Scissor" || 
+        winner === "You win. Paper beats Rock" ||
+        winner === "You win. Scissor beats Paper") {
         return humanScore++;
     } else if (
-        winner === "Computer Wins. Rock beats Scissor" || 
-        winner === "Computer Wins. Paper beats Rock" ||
-        winner === "Computer Wins. Scissor beats Paper") {
+        winner === "You lose. Rock beats Scissor" || 
+        winner === "You lose. Paper beats Rock" ||
+        winner === "You lose. Scissor beats Paper") {
         return computerScore++;
 
     } else {
@@ -86,11 +89,24 @@ function score (winner) {
     }
 }
 
-// console.log (score (playRound (capitalize (userOption), (getComputerChoice(x)))), humanScore, computerScore);
-
 score (playRound (capitalize (userOption), (getComputerChoice(x))));
 
-// console.log (humanScore, computerScore);
+}
 
+// Definying the overall winner
 
-console.log ("Human Score: " + (humanScore) + " Computer Score: " + computerScore);
+function overallWinner (hScore,cScore) {
+    if (hScore > cScore) {
+        return "You win! =D";
+    } else if (hScore < cScore) {
+        return "You lose! :'(";
+    } else {
+        return "You tied! :|";
+    }
+}
+
+console.log (overallWinner(humanScore,computerScore));
+
+// Logging the final result
+
+console.log ("Final Result: Human Score: " + (humanScore) + " Computer Score: " + computerScore);
